9ee959fd4ce49d1a3178cc2d55e3210ee63b5452
Wongsapat <wongsapatup@gmail.com>
2022-04-09 00:30:29 +0700

    practice java script cal

---
+++ cal.js
@@ -0,0 +1,17 @@
+var numField1 = document.getElementById('numField1');
+var numField2 = document.getElementById('numField2');
+var resultField = document.getElementById('resultField');
+var form = document.getElementById('xIs');
+
+form.addEventListener('submit',function(event){
+    if(!numField1.value || !numField2.value){
+        alert("Please Enter values in the fields");
+    }else{
+    var x = parseFloat(numField1.value);
+    var y = parseFloat(numField2.value);
+    var result = x/y;
+    var percent = result * 100;
+    resultField.innerHTML = "Answer " + percent + " %";
+    event.preventDefault();
+    }
+});
